485a2594289a53b34e0d7f759f46f8f289781f13
Dominique HAAS <dominique@hubwa.re>
2019-11-22 23:50:41 +0100

    update code

--- pages/index.vue
+++ pages/index.vue
@@ -27,8 +27,7 @@ export default {
       url: "",
       takingScreenshot: false,
       upload: false,
-      imagesrc:
-        "https://images.prismic.io/dominique-haasfr/c0999fc58f823ee79869225924a04bafefd163d8_emoji.jpg?auto=compress,format"
+      imagesrc: null
     };
   },
   methods: {
